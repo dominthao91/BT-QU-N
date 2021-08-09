428f73a7d74e4d6ba07dfbf885e7d0e7014416d8
dominthao91 <domintaopt@gmail.com>
2021-08-09 08:24:34 +0700

    add

---
+++ src/XdHamThemSpVaoMang.java
@@ -0,0 +1,2 @@
+public class XdHamThemSpVaoMang {
+}
---
+++ src/XdHamTimKiemSpTrongMang.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class XdHamTimKiemSpTrongMang {
+    public static void main(String[] args) {
+        String string;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhập vào các sản phẩm");
+        string =scanner.nextLine();
+    }
+}
+
